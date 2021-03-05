e3ea58eb9370d268872d626f931d45cf859ee666
Hakan Durgay <hakandurgay@gmail.com>
2021-03-05 21:29:00 +0300

    1) eş zamanlı üç tane task oluşturma

---
+++ ParallelProgramming/ParallelProgramming/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelProgramming
+{
+    class Program
+    {
+        public static void Write(char c)
+        {
+            int i = 1000;
+            while (i-- > 0)
+            {
+                Console.Write(c);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Task.Factory.StartNew(() => Write('.'));
+
+            var t=new Task(()=>Write('?'));
+            t.Start();
+
+
+            Write('-');
+
+            Console.ReadKey();
+        }
+    }
+}
