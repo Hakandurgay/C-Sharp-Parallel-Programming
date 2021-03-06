c7cfd09205403c795d9a2ae234b179273eeb034a
Hakan Durgay <hakandurgay@gmail.com>
2021-03-06 13:50:50 +0300

    4) farklı token'ları tek bir token'a bağlayarak bunlardan herhangi biri cancel olduğunda ana token'dan hata fırlatma

--- ParallelProgramming/ParallelProgramming/Program.cs
+++ ParallelProgramming/ParallelProgramming/Program.cs
@@ -14,54 +14,29 @@ namespace ParallelProgramming
      
         static void Main(string[] args)
         {
-            var cts = new CancellationTokenSource();  //task'den çıkılmasını sağlar
-            var token = cts.Token;
+         
+            //bu üç token paranoid tokena bağlı. bu üçünden biri cancel edilirse paranoid token hata fırlatır
+          var planned= new CancellationTokenSource();   
+          var preventative= new CancellationTokenSource();
+          var emergency= new CancellationTokenSource();
 
-            token.Register(() =>
-            {
-                Console.WriteLine("Cancelation has been requested");
-            });
+          var paranoid = CancellationTokenSource.CreateLinkedTokenSource(
+              planned.Token, preventative.Token, emergency.Token);
 
-            var t = new Task(() =>
-            {
-                int i = 0;
-                while (true)
-                {
+          Task.Factory.StartNew(() =>
+          {
+              int i = 0;
+              while (true)
+              {
+                  paranoid.Token.ThrowIfCancellationRequested();
+                  Console.WriteLine($"{i++}\t");
+              }
+          },paranoid.Token);
 
-                    token.ThrowIfCancellationRequested();
-                    Console.WriteLine($"{i++}\t ");
+          Console.ReadKey();
+          emergency.Cancel();
 
-
-                    #region task'den bu şekilde de çıkılabilir ama yukarıdaki daha uygun
-
-                    //if (token.IsCancellationRequested)
-                    //    break;
-                    //else
-                    //{
-                    //    Console.WriteLine($"{i++}\t ");
-                    //}
-
-                    #endregion
-
-                }
-            }, token);
-            t.Start();
-
-            Task.Factory.StartNew(() =>
-            {
-                token.WaitHandle.WaitOne(); //yukarıdaki task beklemeye alınınca bu çalışır.
-                Console.WriteLine("wait handle çalıştı. task'dan çıkıldı");
-            });
-
-            Console.ReadKey();
-            cts.Cancel();  //klavyeden değer okumadan sonra cts.cansel ile task durdurulur
-
-            Console.ReadKey();
-            Console.ReadKey();
-
-
-
-
+          Console.ReadKey();
         }
     }
 }
