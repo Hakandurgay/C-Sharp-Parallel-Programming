def74fd5191258ef7b608c6db87b2d9311907bf2
Hakan Durgay <hakandurgay@gmail.com>
2021-03-06 13:39:28 +0300

    3) çalışan task'den çıkıp yapma ve ayrı bir task ile çıkış yapınca exception fırlatma

--- ParallelProgramming/ParallelProgramming/Program.cs
+++ ParallelProgramming/ParallelProgramming/Program.cs
@@ -1,42 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace ParallelProgramming
 {
     class Program
     {
-        public static void Write(char c)
-        {
-            int i = 1000;
-            while (i-- > 0)
-            {
-                Console.Write(c);
-            }
-        }
-
-        public static int TextLenght(object o)
-        {
-            Console.WriteLine(($"Task with id {Task.CurrentId } processing object {o}...")); //current id çalışan task numarasını verir
-            return o.ToString().Length;
-        }
-
+     
         static void Main(string[] args)
         {
-            string text1 = "testing", text2 = "this";
+            var cts = new CancellationTokenSource();  //task'den çıkılmasını sağlar
+            var token = cts.Token;
 
-            var task1=new Task<int>(TextLenght,text1); // bu şekilde veya factory kullanılarak task oluşturulabilir.
-            task1.Start();
+            token.Register(() =>
+            {
+                Console.WriteLine("Cancelation has been requested");
+            });
 
-            Task<int> task2 =Task<int>.Factory.StartNew(TextLenght, text2);  //otomatik olarak başlatmaması için task 2 ye atandı
+            var t = new Task(() =>
+            {
+                int i = 0;
+                while (true)
+                {
 
-            Console.WriteLine($"Length of '{text1}' is {task1.Result}");  //result kullanınca taskin bitmesini bekler ve bitince sonucu yazdırır
-            Console.WriteLine($"Length of '{text2}' is {task2.Result}");
+                    token.ThrowIfCancellationRequested();
+                    Console.WriteLine($"{i++}\t ");
 
+
+                    #region task'den bu şekilde de çıkılabilir ama yukarıdaki daha uygun
+
+                    //if (token.IsCancellationRequested)
+                    //    break;
+                    //else
+                    //{
+                    //    Console.WriteLine($"{i++}\t ");
+                    //}
+
+                    #endregion
+
+                }
+            }, token);
+            t.Start();
+
+            Task.Factory.StartNew(() =>
+            {
+                token.WaitHandle.WaitOne(); //yukarıdaki task beklemeye alınınca bu çalışır.
+                Console.WriteLine("wait handle çalıştı. task'dan çıkıldı");
+            });
 
             Console.ReadKey();
+            cts.Cancel();  //klavyeden değer okumadan sonra cts.cansel ile task durdurulur
+
+            Console.ReadKey();
+            Console.ReadKey();
+
+
+
+
         }
     }
 }
