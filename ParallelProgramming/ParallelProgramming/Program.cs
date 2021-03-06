f1d3a22a484ff6224201ed304a17a22f49f174bc
Hakan Durgay <hakandurgay@gmail.com>
2021-03-06 14:29:57 +0300

    6) Task.WaitAll ve Task.WaitAny kullanımı

--- ParallelProgramming/ParallelProgramming/Program.cs
+++ ParallelProgramming/ParallelProgramming/Program.cs
@@ -20,11 +20,28 @@ namespace ParallelProgramming
             var token = cts.Token;
             var t = new Task(() =>
             {
-                Console.WriteLine("press any key to disarm: you have 5 seconds");
-                bool cancelled = token.WaitHandle.WaitOne(5000);
-                Console.WriteLine(cancelled? "bomb disarmed." : "boom");
+                Console.WriteLine("5 sanyie süren task");
+                for (int i = 0; i < 5; i++)
+                {
+                    token.ThrowIfCancellationRequested();
+                    Thread.Sleep(1000);
+                } 
+                Console.WriteLine("5 saniyelik task bitti");
+
             },token);
             t.Start();
+
+            Task t2 = Task.Factory.StartNew(() => Thread.Sleep(3000), token);
+
+              //      Task.WaitAll(t, t2); //threadlerin ikisi de bitene kadar bekler.
+             //   Task.WaitAny(t2); //thread bitene kadar bekler sonra devam eder
+            //    Task.WaitAny(t2,t); //ilk hangisi bitiyorsa onu bekler
+            Task.WaitAll(new[] {t, t2}, 4000, token); //4 saniye sonra devam etttir
+
+            Console.WriteLine($"task t status is {t.Status}" );
+            Console.WriteLine($"task t2 status is {t2.Status}" );
+
+            Console.WriteLine("Main program done");
             Console.ReadKey();
             cts.Cancel();
 
