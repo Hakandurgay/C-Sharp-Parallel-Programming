626415cd43ae525af4484693e952e59434b80ebd
Hakan Durgay <hakandurgay@gmail.com>
2021-03-07 13:37:43 +0300

    7) critical section probleminin çözümü: lock keywordü kullanma

--- ParallelProgramming/ParallelProgramming/Program.cs
+++ ParallelProgramming/ParallelProgramming/Program.cs
@@ -10,42 +10,59 @@ using System.Xml;
 
 namespace ParallelProgramming
 {
+
+    public class BankAccount
+    {
+        //birden basla thread t anında n tane işlem yapabilme ihtimali olduğu için lock keywordu uygulanmazsa critical section oluşur. yani 
+        // program her çalıştığında sonuç sıfır olması gerekirken farklı sonuçlar elde edilebilirç
+        //bunu önlemek için lock keywordu kullanılır. Bir thread içeri girdiğinde diğeri de girmek isterse ilk giren bitene kadar bekler.
+        //böylece hepsi sırayla girerek bir işlemde birden fazla thread çalışması engellenir.
+        public object padlock=new object();
+        public int Balance  { get; set; }
+
+        public void Deposit(int amount)
+        {
+            lock (padlock)
+            {
+                Balance += amount;
+            }
+        }
+
+        public void Withdraw(int amount)
+        {
+            lock (padlock)
+            {
+              Balance -= amount;
+            }
+        }
+    }
     class Program
     {
-     
         static void Main(string[] args)
         {
+            var ba = new BankAccount();
+            var tasks=new List<Task>();
 
-            var cts =new CancellationTokenSource();
-            var token = cts.Token;
-            var t = new Task(() =>
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("5 sanyie süren task");
-                for (int i = 0; i < 5; i++)
+                tasks.Add(Task.Factory.StartNew(() =>
                 {
-                    token.ThrowIfCancellationRequested();
-                    Thread.Sleep(1000);
-                } 
-                Console.WriteLine("5 saniyelik task bitti");
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        ba.Deposit(100);
+                    }
+                }));
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 1000; j++)
+                    {
+                        ba.Withdraw(100);
+                    }
+                }));
+            }
 
-            },token);
-            t.Start();
-
-            Task t2 = Task.Factory.StartNew(() => Thread.Sleep(3000), token);
-
-              //      Task.WaitAll(t, t2); //threadlerin ikisi de bitene kadar bekler.
-             //   Task.WaitAny(t2); //thread bitene kadar bekler sonra devam eder
-            //    Task.WaitAny(t2,t); //ilk hangisi bitiyorsa onu bekler
-            Task.WaitAll(new[] {t, t2}, 4000, token); //4 saniye sonra devam etttir
-
-            Console.WriteLine($"task t status is {t.Status}" );
-            Console.WriteLine($"task t2 status is {t2.Status}" );
-
-            Console.WriteLine("Main program done");
-            Console.ReadKey();
-            cts.Cancel();
-
-          Console.ReadKey();
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine($"final balance is: {ba.Balance}");
         }
     }
 }
