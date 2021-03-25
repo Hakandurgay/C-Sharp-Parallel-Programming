7af8683d252e4996ccb164e02f9241d201f6dcad
Hakan Durgay <hakandurgay@gmail.com>
2021-03-25 23:52:57 +0300

    27) async-await kullanımı

---
+++ ParallelProgramming/AsyncAwaitExamples/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwaitExamples
+{
+    public partial class Form1 : Form
+    {
+        public async Task<int> CalculateValueAsync() //bu şekilde yapılırsa bekleme işlemi olurken ui kısmında işlem yapılabilir. 
+        {
+          await  Task.Delay(5000);   //async arkaplanda özellikle bir iş yapmaz sadece await'in kullanılacağını belirtir
+            return  123;
+        }
+        public Form1()
+        { 
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var calculation =await CalculateValueAsync();
+            label1.Text = calculation.ToString();
+
+            //await kullanıldığı için bu şekilde uzun yazmaya gerek kalmadı
+            //calculation.ContinueWith(t =>
+            //{
+            //    label1.Text = t.Result.ToString();
+
+            //}, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- ParallelProgramming/ParallelProgramming/Program.cs
+++ ParallelProgramming/ParallelProgramming/Program.cs
@@ -18,40 +18,10 @@ namespace ParallelProgramming
   public  class Program
     {
       
+
         static void Main(string[] args)
         {
-            var items = ParallelEnumerable.Range(1, 20); //geriye paralel query döndürür
-            var cts =new CancellationTokenSource();
-            var results = items.Select(i =>
-            {
-                double result = Math.Log10(i);
-             //   if(result>1) throw new InvalidOperationException(); //bu şekilde hata fırlatılabilir
-
-                Console.WriteLine($"i={i}, tid={Task.CurrentId}");
-                return result;
-            });
-            try
-            {
-                foreach (var x in results)
-                {
-                    if (x > 1)
-                        cts.Cancel();
-
-                    Console.WriteLine($"result={x}");
-                }
-            }
-            catch (AggregateException ae)
-            {
-                ae.Handle(e =>
-                {
-                    Console.WriteLine($"{e.GetType().Name}: {e.Message}");
-                    return true;
-                });
-            }
-            catch (OperationCanceledException e)
-            {
-                Console.WriteLine("canceled");
-            }
+            
         }
     }
 }
